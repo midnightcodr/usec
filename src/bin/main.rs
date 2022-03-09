cfc78dc31bece205ab6a89bbd35aff3087348574
Rico Chen <midnightcodr@gmail.com>
2022-03-08 23:52:10 -0500

    using example

--- src/bin/main.rs
+++
@@ -1,8 +0,0 @@
-use fin::calendar::UsExchangeCalendar;
-fn main() {
-    println!("let's do it");
-    let mut sc = UsExchangeCalendar::with_default_rules(false);
-    sc.populate_cal(Some(2021), Some(2024));
-    let c = sc.get_cal();
-    println!("{:?}", c);
-}
